dad36d9f95035d616749fbffbffd6e3b5087ec6b
Ivan Badicean <badican01117@gmail.com>
2024-03-21 10:43:32 +0200

    Extend vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
+import { compression } from 'vite-plugin-compression2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [
+    react(),
+    legacy(),
+    compression(),
+    compression({ algorithm: 'brotliCompress', exclude: [/\.(br)$/, /\.(gz)$/] }),
+  ],
+  ssr: {
+    noExternal: ['react-helmet-async'],
+  },
 })
